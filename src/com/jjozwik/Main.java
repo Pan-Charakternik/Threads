f2eb506c188f606b044a6d64b6868e6d37b824fb
Pan-Charakternik <35796629+Pan-Charakternik@users.noreply.github.com>
2018-05-14 14:44:49 +0200

    Update 3

--- src/com/jjozwik/Main.java
+++ src/com/jjozwik/Main.java
@@ -23,10 +23,17 @@ public class Main {
             @Override
             public void run() {
                 System.out.println(ANSI_RED + "Hello from the anonymous class implementation of thread");
+                try{
+                    anotherThread.join();
+                    System.out.println(ANSI_RED + "AnotherThread terminated, or timed out, so I'm running again");
+                }catch (InterruptedException e){
+                    System.out.println(ANSI_RED + "I couldn't wait after all. I was interrupted");
+                }
             }
         });
 
         myRunnableThread.start();
+//        anotherThread.interrupt();
 
         System.out.println(ANSI_PURPLE + "Hello again form the main thread.");
 
