d9858b065e5f3ad4870e2216040ba07a426d1c04
Pan-Charakternik <35796629+Pan-Charakternik@users.noreply.github.com>
2018-05-14 11:59:29 +0200

    Update 1

---
+++ src/com/jjozwik/AnotherThread.java
@@ -0,0 +1,10 @@
+package com.jjozwik;
+
+import static com.jjozwik.ThreadColor.ANSI_BLUE;
+
+public class AnotherThread extends Thread {
+    @Override
+    public void run() {
+        System.out.println(ANSI_BLUE + "Hello from another thread.");
+    }
+}
--- src/com/jjozwik/Main.java
+++ src/com/jjozwik/Main.java
@@ -1,8 +1,24 @@
 package com.jjozwik;
 
+import static com.jjozwik.ThreadColor.ANSI_GREEN;
+import static com.jjozwik.ThreadColor.ANSI_PURPLE;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(ANSI_PURPLE + "Hello from the main thread.");
+
+        Thread anotherThread = new AnotherThread();
+        anotherThread.start();
+
+        new Thread() {
+            public void run(){
+                System.out.println(ANSI_GREEN + "Hello from the anonymous class thread.");
+            }
+        }.start();
+
+        System.out.println(ANSI_PURPLE + "Hello again form the main thread.");
+
+
     }
 }
